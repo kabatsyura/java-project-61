054dafbb9e031cf0dd0e3e97effde9b3ddae5ae0
Дмитрий Кабацюра <cauliflb@student.21-school.ru>
2024-01-06 12:16:02 +0300

    Task 9 is done. Added a game Prime

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         int numberOfGames = 3;
-
         Scanner game = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter."
                 + "\n1 - Greet\n"
